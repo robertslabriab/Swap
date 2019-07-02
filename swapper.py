7d658eca211fda1f9cd5b28103b795673c0fbbf9
robertslabriab <robertslabriab@gmail.com>
2019-07-01 21:08:40 -0400

    Created swapper to swap letters in message

---
+++ swapper.py
@@ -0,0 +1,15 @@
+def swapper(letters,message):
+	letterOne=letters[0].lower()
+	letterTwo=letters[1].lower()
+	msg=list(message)
+	for x in range(len(msg)):
+		if msg[x].lower()==letterOne:
+			msg[x]=letterTwo
+		elif msg[x].lower()==letterTwo:
+			msg[x]=letterOne
+	msg="".join(msg)
+	return msg
+
+
+#cd\Portfolio\swap
+#swapper.py
