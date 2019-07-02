f0521b6e59aaee21957da371d5264cbfb992c64e
robertslabriab <robertslabriab@gmail.com>
2019-07-01 23:06:32 -0400

    Fixed swapInput to work better with key.py and prevent errors from occuring when only one letter/number is entered

--- swapInput.py
+++ swapInput.py
@@ -1,12 +1,8 @@
 def swapLetters():
-	letters=input("What two letters would you like to swap?\n")
-	letterOne=letters[0].lower()
-	letterTwo=letters[1].lower()
+	letters=input("What two letters/numbers would you like to swap?\n")
 	
-	while len(letters)!=2 or letterOne==letterTwo:
-		letters=input("Please enter two different letters.\n")
-		letterOne=letters[0].lower()
-		letterTwo=letters[1].lower()
+	while len(letters)!=2 or letters[0].lower()==letters[1].lower():
+		letters=input("Please enter two different letters/numbers.\n")
 	return letters
 
 def swapMessage():
