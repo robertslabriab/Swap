e64b5f19cbd46c34c1695d48d39f353269901a70
robertslabriab <robertslabriab@gmail.com>
2019-07-03 10:25:49 -0400

    added key to swapping to allow for multiswapping based of keys entered

--- swapping.py
+++ swapping.py
@@ -1,6 +1,18 @@
-import swapInput,swapper
+import swapInput,swapper,key
 def swapping():
-	swappedMessage=swapper.swapper(swapInput.swapLetters(),swapInput.swapMessage())
-	print(swappedMessage)
 
-swapping()
+	positiveAnswer=["yes","y"]
+	negativeAnswer=["no","n"]
+	haveKey=input("Do you have a key?\n")
+	if haveKey.lower() in positiveAnswer:
+		keyList=key.keyFob()
+		tracker=0
+		while tracker<len(keyList):
+			if tracker==0:
+				swappedMessage=swapper.swapper(keyList[tracker],swapInput.swapMessage())
+			else:
+				swappedMessage=swapper.swapper(keyList[tracker],swappedMessage)
+			tracker+=1
+	else:
+		swappedMessage=swapper.swapper(swapInput.swapLetters(),swapInput.swapMessage())
+	print(swappedMessage)
