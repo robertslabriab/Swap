af01a590265d61ece9d61b7d1f56b44f6e09ff91
robertslabriab <robertslabriab@gmail.com>
2019-07-03 10:24:42 -0400

    Switched the names of variables: numOfKeys and keys

--- key.py
+++ key.py
@@ -1,19 +1,17 @@
 import swapInput
 def keyFob():
-	keys=input("How many keys would you like?\n")
-	numOfKeys=[]
+	numOfKeys=input("How many keys would you like?\n")
+	keys=[]
 	flag=False
 	tracker=0
-	
-	if keys.isnumeric()==True:
+	if numOfKeys.isnumeric()==True:
 		flag=True
-	
-	while keys.isnumeric()==False:
-		keys=input("Please input a number.\n")
-		if keys.isnumeric()==True:
+	while numOfKeys.isnumeric()==False:
+		numOfKeys=input("Please input a number.\n")
+		if numOfKeys.isnumeric()==True:
 			flag=True
-	while tracker<int(keys) and flag==True:
+	while tracker<int(numOfKeys) and flag==True:
 		newKey=swapInput.swapLetters()
-		numOfKeys.append(newKey)
+		keys.append(newKey)
 		tracker+=1
-	return numOfKeys
+	return keys
